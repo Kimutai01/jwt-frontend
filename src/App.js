9a8dc96f421259c47c70d32de442fe1bb38c1102
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-27 14:46:12 +0300

    Adding login logout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const API = "http://localhost:3000";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`${API}/api/v1/users`, {
+      method: "POST",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          email,
+          password,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+    fetch(`${API}/api/v1/login`, {
+      method: "POST",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: loginUsername,
+          password: loginPassword,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+
+    setLoginUsername("");
+    setLoginPassword("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create new user</h1>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="text"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+
+      <h1>LOgin</h1>
+
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="name"
+            value={loginUsername}
+            onChange={(e) => setLoginUsername(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Password:
+          <input
+            type="text"
+            name="password"
+            value={loginPassword}
+            onChange={(e) => setLoginPassword(e.target.value)}
+          />
+        </label>
+        <button onClick={submitLogin}>Submit</button>
+      </form>
     </div>
   );
 }
